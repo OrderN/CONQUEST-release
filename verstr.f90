9154115f7f76615ca48f9c29e34b84e0e9dcb597
David Bowler <david.bowler@ucl.ac.uk>
2016-01-29 15:30:30 +0000

    ion_electrostatic_module.f90
    Changed name of module for ion-ion electrostatics

--- verstr.f90
+++ verstr.f90
@@ -1,1 +1,1 @@
-  character(len=*), parameter :: commentver='Version beta 0.49b (rRRR): non-SCF stress drb 2015/09/04'
+  character(len=*), parameter :: commentver='Version beta 0.50 (rRRR): neutral atom drb, tm 2016/01/29'
