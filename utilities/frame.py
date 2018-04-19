66251fae9210172e9450da2c6cda39aa758b4dc5
Zamaan Raza <zamaan.raza@gmail.com>
2018-04-19 10:21:40 +0900

    removed some more guff from md_analysis scripts

--- utilities/frame.py
+++ utilities/frame.py
@@ -75,35 +75,3 @@ class Frame:
       bits.pop(0)
       for j in range(3):
         self.f[i,j] = float(bits[j])
-
-  def update_vacf(self, init):
-    """Compute the velocity autocorrelation function given the initial frame"""
-    return sp.sum(init.v * self.v)/self.nat
-
-  def diff_mic(self, v1, v2):
-    diff = v2 - v1
-    for i in range(3):
-      diff[i] = diff[i] - round(diff[i]/self.lat[i,i])
-    return diff
-
-  def update_msd(self, init):
-    """Compute the mean squared displacement given the initial frame"""
-    diff = sp.zeros((self.nat,3))
-    for i in range(self.nat):
-      diff[i,:] = self.diff_mic(self.r[i,:], init.r[i,:])
-    return sp.sum(diff**2)/self.nat
-
-  def update_vdistr(self, nbins):
-    """Generate the velocity distribution histogram"""
-    vdistr = sp.zeros((nbins,3), dtype='float')
-    for i in range(3):
-      hist, bin_edges = sp.histogram(self.v[:,i], nbins, range=(0,self.vmax))
-      vdistr[:,i] = hist
-    return vdistr, bin_edges
-
-  def update_sdistr(self, nbins):
-    """Generate the speed distribution histogram"""
-    speed = sp.zeros(self.nat, dtype='float')
-    for i in range(self.nat):
-      speed[i] = sp.sum(self.v[i,:]**2)
-    return sp.histogram(speed, nbins, range=(0,self.vmax))
