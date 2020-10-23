5e2fa93606e469cc41d0059c6915b0810a3f3ea2
lionelalexandre <lionel.truflandier@gmail.com>
2020-10-23 18:14:01 +0200

    added evaluate_pao in Cartesian

--- src/exx_evalpao.f90
+++ src/exx_evalpao.f90
@@ -15,6 +15,10 @@ contains
 !!   2016/12/29 21:30 nakata
 !!    Used pao information instead of blips_on_atom, and 
 !!    removed support_spec_format (blips_on_atom), which is no longer needed
+!!   2019/01/23 11:20 lionel
+!!    pao on grid are now evaluated from evaluate_pao in ol_ang_coeff_subs.f90 
+!!    Cartesian system is preferred in this case (faster) cf. exx_cartesian = T/F
+
   subroutine exx_phi_on_grid(inode,atom,spec,extent,xyz,nsuppfuncs,phi_on_grid,r_int,rst)
 
     use numbers,      only: zero, one, two, three, four, five, six, fifteen, sixteen
@@ -43,7 +47,7 @@ contains
          intent(inout) :: phi_on_grid
 
     ! << Local variables >>
-    real(double), dimension(nsuppfuncs) :: sfsum
+    !real(double), dimension(nsuppfuncs) :: sfsum
 
     integer             :: nx, ny, nz 
     integer             :: mx, my, mz   
@@ -76,7 +80,7 @@ contains
     ngrid        = 2*extent+1
     grid_spacing = r_int/real(extent,double)                   
     phi_on_grid  = zero
-    sfsum        = zero
+    !sfsum        = zero
 
     mx = -extent ; px = extent
     my = mx      ; py = px 
@@ -163,7 +167,7 @@ contains
              !print*, '1 cycle start'
 
              count1  = 1
-             sfsum   = zero
+             !sfsum   = zero
              angu_loop: do l1 = 0, pao(spec)%greatest_angmom
 
                 zeta_loop: do acz = 1, pao(spec)%angmom(l1)%n_zeta_in_angmom
@@ -172,79 +176,8 @@ contains
 
                       pao_val = zero
                       y_val   = zero             
-                      rec_ylm: if (.not.exx_cartesian) then                      
-                         call evaluate_pao(spec,l1,acz,m1,x,y,z,pao_val)
-                      else
-                         npts  = pao(spec)%angmom(l1)%zeta(acz)%length
-                         del_r = (pao(spec)%angmom(l1)%zeta(acz)%cutoff/&
-                              (pao(spec)%angmom(l1)%zeta(acz)%length-1))
-
-                         n1 = floor(r/del_r)
-                         n2 = n1 + 1
-
-                         if(n2 > npts-1) then
-                            pao_val = 0.0_double
-
-                         else
-                            xj1 = n1  * del_r
-                            xj2 = xj1 + del_r
-
-                            a = (xj2 - r)/del_r
-                            b = 1.0_double - a
-                            c = a*(a*a - 1.0_double)*del_r*del_r/6.0_double
-                            d = b*(b*b - 1.0_double)*del_r*del_r/6.0_double
-
-                            a_table = pao(spec)%angmom(l1)%zeta(acz)%table(n1+1)
-                            b_table = pao(spec)%angmom(l1)%zeta(acz)%table(n2+1)
-                            c_table = pao(spec)%angmom(l1)%zeta(acz)%table2(n1+1)
-                            d_table = pao(spec)%angmom(l1)%zeta(acz)%table2(n2+1)
-
-                            pao_val = a*a_table + b*b_table + c*c_table + d*d_table
-
-                            if (l1 == 0) then !s-type function
-                               y_val = a1g_norm
-
-                            else if(l1 == 1) then !p-type function
-                               r_p: if (r > zero) then                               
-                                  select case(m1)
-                                  case( 1) !p_x
-                                     y_val = t1u_norm*x
-                                  case( 0) !p_z
-                                     y_val = t1u_norm*z 
-                                  case(-1) !p_y
-                                     y_val = t1u_norm*y 
-                                  case default
-                                     call cq_abort('exx_phi_on_grid/Unrecognized magn. m ',m1)
-                                  end select
-                               else
-                                  y_val = zero
-                               end if r_p
-                            else if(l1 == 2) then !d-type function                                 
-                               r_d: if (r > zero) then
-                                  select case(m1)
-                                  case( 2) ! d_{x2-y2}
-                                     y_val =  eg_a_norm*(x*x-y*y) 
-                                  case( 1) ! d_{xz}
-                                     y_val =  t2g_norm*(x*z)         
-                                  case( 0) ! d_{z2}
-                                     y_val =  eg_b_norm*(3.0d0*z*z-r*r)
-                                  case(-1) ! d_{yz}
-                                     y_val =  t2g_norm*(y*z)
-                                  case(-2) ! d_{yz}             
-                                     y_val = -t2g_norm*(x*y) ! Take care phase factor
-                                  case default
-                                     call cq_abort('exx_phi_on_grid/Unrecognized mag. m ',m1)
-                                  end select
-                               else
-                                  y_val = zero
-                               end if r_d
-                            else                               
-                               call cq_abort('exx_phi_on_grid/Unrecognized angul. l ',l1)
-                            end if
-
-                            pao_val = pao_val*y_val
-                         end if
-                      end if rec_ylm
+                         
+                      call evaluate_pao(spec,l1,acz,m1,x,y,z,pao_val,.false.)
 
                       ! Put pao_val directly into phi_on_grid
                       ! (only for primitive PAOs and not for blips)
