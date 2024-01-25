dfb30e6a27774fee429b02c24c704aca278862d7
Tuomas Koskela <t.koskela@ucl.ac.uk>
2024-01-25 18:06:02 +0000

    Avoid loop carrier dependencies by precomputing indices

--- src/multiply_kernel_ompGemm_m.f90
+++ src/multiply_kernel_ompGemm_m.f90
@@ -168,7 +168,7 @@ contains
     integer :: nbbeg, nbend, tbbeg, tbend
     external :: dgemm
     ! OpenMP required indexing variables
-    integer :: nd1_1st(at%mx_halo), nd2_1st(mx_absb)
+    integer :: nd1_vector(at%mx_halo), nd2_vector(mx_absb), nd2_array(mx_absb)
 
     ! Allocate tempa, tempb, tempc to largest possible size outside the loop
     maxnd1 = maxval(ahalo%ndimi)
@@ -188,41 +188,43 @@ contains
        nbkbeg = ibaddr(k_in_part)
        nb_nd_kbeg = ib_nd_acc(k_in_part)
        nd3 = ahalo%ndimj(k_in_halo)
-       ! for OpenMP sub-array indexing
-       nd1_1st(1) = 0
+
+       ! Precompute indices that depend on i and j to avoid loop carrier
+       ! dependency in the following loops
+       nd1_vector(1) = 0
        do i = 2, at%n_hnab(k_in_halo)
-         i_in_prim = at%i_prim(at%i_beg(k_in_halo)+i-2)
-         nd1_1st(i) = nd1_1st(i-1) + nd3 * ahalo%ndimi(i_in_prim)
+         i_in_prim = at%i_prim(at%i_beg(k_in_halo) + i - 2)
+         nd1_vector(i) = nd1_vector(i-1) + nd3 * ahalo%ndimi(i_in_prim)
        end do
-       nd2_1st(1) = 0
+       nd2_vector(1) = nb_nd_kbeg
+       nd2_array(1) = 1
        do j = 2, nbnab(k_in_part)
-          nd2_1st(j) = nd2_1st(j-1) + nd3 * bndim2(nbkbeg+j-2)
+          nd2 = bndim2(nbkbeg + j - 2)
+          nd2_vector(j) = nd2_vector(j - 1) + nd3 * nd2
+          nd2_array(j) = nd2_array(j - 1) + nd2
        end do
 
-       tbbeg = 0
-       tbend = 0
        ! transcription of j from partition to C-halo labelling
-       do j = 1, nbnab(k_in_part)
-          jpart = ibpart(nbkbeg+j-1) + k_off
-          jseq = ibseq(nbkbeg+j-1)
-          jbnab2ch(j) = chalo%i_halo(chalo%i_hbeg(jpart)+jseq-1)
+       copy_b: do j = 1, nbnab(k_in_part)
+          jpart = ibpart(nbkbeg + j - 1) + k_off
+          jseq = ibseq(nbkbeg + j - 1)
+          jbnab2ch(j) = chalo%i_halo(chalo%i_hbeg(jpart) + jseq - 1)
 
           nd2 = bndim2(nbkbeg+j-1)
-          nbbeg = nb_nd_kbeg + nd2_1st(j)
-
-          nbend = nbbeg + (nd3 * nd2 - 1)
-          tbbeg = tbend + 1
-          tbend = tbend + nd2
-          ! Loop over B-neighbours of atom k
+          nbbeg = nd2_vector(j)
+          nbend = nbbeg + nd3 * nd2 - 1
+          tbbeg = nd2_array(j)
+          tbend = tbbeg + nd2 - 1
+          ! Copy B-neighbours of atom k to temporary array
           tempb(1:nd3, tbbeg:tbend) = reshape(b(nbbeg:nbend), [nd3, nd2])
-       end do
-!$omp do schedule(runtime)
+       end do copy_b
+
        ! Loop over primary-set A-neighbours of k
        do i = 1, at%n_hnab(k_in_halo)
           i_in_prim = at%i_prim(at%i_beg(k_in_halo)+i-1)
           icad = (i_in_prim - 1) * chalo%ni_in_halo
           nd1 = ahalo%ndimi(i_in_prim)
-          nabeg = at%i_nd_beg(k_in_halo) + nd1_1st(i)
+          nabeg = at%i_nd_beg(k_in_halo) + nd1_vector(i)
           naend = nabeg + (nd1 * nd3 - 1)
 
           tempa(1:nd1, 1:nd3) = reshape(a(nabeg:naend), [nd1, nd3], order = [2,1])
@@ -230,8 +232,6 @@ contains
           call dgemm('n', 'n', nd1, tbend, nd3, one, tempa, &
                maxnd1, tempb, maxnd3, zero, tempc, maxnd1)
 
-          tcbeg = 0
-          tcend = 0
           ! Loop over B-neighbours of atom k
           ! TODO: Create a mask for the rows/cols of C that we want and do array copy
           copy_c: do j = 1, nbnab(k_in_part)
@@ -243,6 +243,8 @@ contains
              if (jbnab2ch(j) /= 0 .and. ncbeg /= 0) then
                 c(ncbeg:ncend) = c(ncbeg:ncend) + pack(tempc(1:nd1, tcbeg:tcend), .true.)
              end if
+             tcbeg = nd2_array(j)
+             tcend = tcbeg + nd2 - 1
           end do copy_c
 
           ! sofar = 0
