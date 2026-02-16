663200699ade750b91159d5013a538b535dcfe4a
Lionel <lionel.truflandier@gmail.com>
2026-02-16 16:46:31 +0100

    tutorial 1 test 3

---
+++ tutorials/tutorial_1/3.test_Cu_kpointsmesh/loop_kmesh_runCQ.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Set env variables
+source ../../environment_variables.sh
+
+# Setup Conquest command
+CQCMD="$CQMPI $CQNUM $CQROOT/Conquest"
+echo "Conquest cmd: $CQCMD"
+
+# Ion files
+ions=("Cu.ion")
+
+# List of kpoints
+values=(2 4 6 8)
+
+# Loop over each k-point triplet
+for kp in "${values[@]}"; do
+    folder="kp_${kp}x${kp}x${kp}"
+    if [ ! -d $folder ]; then
+       mkdir -p $folder
+    fi
+    # Copy required input files into the folder
+    cp Cu.ion coords.dat Conquest_input $folder
+    cd $folder
+
+    # Generate Conquest_input file with current k-points value
+    echo "diag.mpmesh  True"  >> Conquest_input
+    echo "diag.mpmeshx ${kp}" >> Conquest_input
+    echo "diag.mpmeshy ${kp}" >> Conquest_input
+    echo "diag.mpmeshz ${kp}" >> Conquest_input
+
+    # Run Conquest in the folder
+    echo -n "Conquest is running in $folder... "
+    $CQCMD > Conquest_out
+    echo "done" 
+    cd ..
+done
+
