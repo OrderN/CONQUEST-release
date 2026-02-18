7d03a755b5aaa70804d6630f5ee5d25a1557733f
Lionel <lionel.truflandier@gmail.com>
2026-02-18 16:00:43 +0100

    added tutorial 1 example conformer phenol

---
+++ tutorials/tutorial_2/2.exercise_phenol_optpos/optpos_runCQ.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set env variables
+source ../../environment_variables.sh
+
+# Setup Conquest command
+CQCMD="$CQMPI $CQNUM $CQROOT/Conquest"
+echo "Conquest cmd: $CQCMD"
+
+# Just run Conquest
+$CQCMD
+
+# Extract data from Conquest output
+./optpos_extCQ.sh
+
+# Plot data using a python script
+python optpos_pltCQ.py
+
+# Clean around but keep Conquest_out and trajectory.xsf
+./optpos_clnCQ.sh
+
+# Launch vmd using a visualisation data file 
+$VMD_CMD trajectory.xsf -e optpos_visCQ.vmd
