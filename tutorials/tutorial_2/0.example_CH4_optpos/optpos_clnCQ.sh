4329827cc0f2e2ecf66b37f1dcafe25fa164a977
Lionel <lionel.truflandier@gmail.com>
2026-02-18 13:33:36 +0100

    Added tutorial 2 example 0

--- tutorials/tutorial_2/0.example_CH4_optpos/optpos_clnCQ.sh
+++ tutorials/tutorial_2/0.example_CH4_optpos/optpos_clnCQ.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# List of cutoff energy values
-values=(50 75 100 120 150)
-
 rm_files=("Conquest_out_ase" 
 "Conquest_warnings"
 "conquest.bib"
@@ -12,7 +9,6 @@ rm_files=("Conquest_out_ase"
 "InfoGlobal.i00"
 "input.log"
 "Kmatrix*"
-"trajectory.xsf"
 "UpdatedAtoms.dat"
 "make_prt.dat")
 
