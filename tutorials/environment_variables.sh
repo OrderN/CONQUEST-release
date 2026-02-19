a61d50a8547277ecbb49e0b9a2ce134c55c317d7
Lionel <lionel.truflandier@gmail.com>
2026-02-19 13:05:22 +0100

    added printing of process/threads when source env.

--- tutorials/environment_variables.sh
+++ tutorials/environment_variables.sh
@@ -4,7 +4,10 @@ CQMPI='mpirun -np'
 export CQMPI
 CQNUM=2
 export CQNUM
-OMP_NUM_THREADS=1
+OMP_NUM_THREADS=2
 export OMP_NUM_THREADS
 VMD_CMD='/Applications/VMD_2.0.0a7-pre2.app/Contents/MacOS/startup.command'
 export VMD_CMD
+
+echo "number of process: $CQNUM"
+echo "number of threads: $OMP_NUM_THREADS"
