7e87661dbe7c2de3c3bf54320418a9aeacbff129
Lionel <lionel.truflandier@gmail.com>
2026-02-19 15:37:21 +0100

    added tutorial 3 example 0

---
+++ tutorials/tutorial_3/1.H2O_molecule/0.NVT/md_runCQ.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Set env variables
+source ../../../environment_variables.sh
+
+# Setup Conquest command
+CQCMD="$CQMPI $CQNUM $CQROOT/Conquest"
+echo "Conquest cmd: $CQCMD"
+
+# Start timer
+start_time="$(date -u +%s.%N)"
+
+# Just run Conquest
+$CQCMD
+
+# End timer
+end_time="$(date -u +%s.%N)"
+
+# Print elapsed time
+elapsed="$(bc <<<"$end_time-$start_time")"
+printf "total of %.3f seconds elapsed\n" "$elapsed"
+
+# Extract data from Conquest output
+
+# Plot data using a python script
+python md_analysis.py -s md.stats
+
+# Clean around but keep Conquest_out/trajectory.xsf 
+# and md files including velocity.dat
+#./md_clnCQ.sh
+
+# Launch vmd using a visualisation data file 
+$VMD_CMD trajectory.xsf -e md_visCQ.vmd
