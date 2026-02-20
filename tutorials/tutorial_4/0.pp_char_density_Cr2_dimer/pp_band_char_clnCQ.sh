a44ec9c751dd3b7c0ca09f5ca884f58912deb27f
Lionel <lionel.truflandier@gmail.com>
2026-02-20 14:10:07 +0100

    added tutorial 4 exmaple 0

---
+++ tutorials/tutorial_4/0.pp_char_density_Cr2_dimer/pp_band_char_clnCQ.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm_files=("Conquest_out_ase" 
+"Conquest_warnings"
+"conquest.bib"
+"coord_next.dat"
+"eigenvalues.dat"
+"hilbert_make_blk.dat"
+"InfoGlobal.i00"
+"input.log"
+"Kmatrix*"
+"UpdatedAtoms.dat"
+"make_prt.dat"
+"chden*"
+"AtomCharge.dat"
+"*WF*")
+
+for f in "${rm_files[@]}"; do
+   rm -f -- $f 
+done
