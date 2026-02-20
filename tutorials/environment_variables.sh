6608faeee18e7a27abbd4a6bc9b4e3af83dd3000
Lionel <lionel.truflandier@gmail.com>
2026-02-20 14:07:29 +0100

    en. var. update

--- tutorials/environment_variables.sh
+++ tutorials/environment_variables.sh
@@ -2,12 +2,15 @@ CQROOT='/Users/lioneltruflandier/CONQUEST-tutorials/bin'
 export CQROOT
 CQMPI='mpirun -np'
 export CQMPI
-CQNUM=2
+CQNUM=1
 export CQNUM
-OMP_NUM_THREADS=2
+OMP_NUM_THREADS=1
 export OMP_NUM_THREADS
 VMD_CMD='/Applications/VMD_2.0.0a7-pre2.app/Contents/MacOS/startup.command'
 export VMD_CMD
+VESTA_CMD='/Applications/VESTA/VESTA.app/Contents/MacOS/VESTA'
+export VESTA_CMD
 
 echo "number of process: $CQNUM"
 echo "number of threads: $OMP_NUM_THREADS"
+
