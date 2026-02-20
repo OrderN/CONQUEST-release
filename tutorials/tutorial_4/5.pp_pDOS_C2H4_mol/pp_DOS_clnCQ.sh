858c7a9318d8fc53536f819d327a27c16af44e41
Lionel <lionel.truflandier@gmail.com>
2026-02-20 14:12:08 +0100

    added tutorial 4 exmaple 5

---
+++ tutorials/tutorial_4/5.pp_pDOS_C2H4_mol/pp_DOS_clnCQ.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rm_files=("Conquest_out_ase" 
+"Conquest_warnings"
+"conquest.bib"
+"coord_next.dat"
+"eigenvalues.dat"
+"hilbert_make_blk.dat"
+"InfoGlobal.i00"
+"input.log"
+"Kmatrix*"
+"UpdatedAtoms.dat"
+"make_prt.dat"
+"chden*"
+"AtomCharge.dat"
+"*cube"
+"*WF*"
+"pp_output"
+"*DOS_lm*")
+
+for f in "${rm_files[@]}"; do
+   rm -f -- $f 
+done
