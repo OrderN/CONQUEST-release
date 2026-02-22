3a39688bc5050216329a98ee59f7fe6db28d7825
Lionel <lionel.truflandier@gmail.com>
2026-02-22 14:10:31 +0100

    added emplate loop scripts

---
+++ tutorials/loop_template/loop_block_runCQ_template.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Set env variables
+source ../environment_variables.sh
+
+# Setup Conquest command
+CQCMD="$CQMPI $CQNUM $CQROOT/Conquest"
+echo "Conquest cmd: $CQCMD"
+
+# List of files to copy including the block
+files=("Si.ion" "coords.dat" "Conquest_input" "block.txt")
+
+# List of kpoints
+values=(1 2 4 6)
+
+# Folder basename
+base="kp_%03d%03d%03d"
+
+# Start timer
+start_time="$(date -u +%s.%N)"
+
+# Loop over each cutoff energy value
+for val in "${values[@]}"; do
+    folder=$(printf "$base" "$val" "$val" "$val")
+    if [ ! -d $folder ]; then
+       mkdir -p $folder
+    fi
+    # Copy required input files into the folder
+    cp "${files[@]}" $folder
+    cd $folder
+
+    # Generate Conquest_input file with current block 
+    while IFS= read -r line; do
+       eval "echo \"$line\"" >> Conquest_input
+    done < block.txt
+
+    # Run Conquest in the folder
+    echo -n "Conquest is running in $folder... "
+    $CQCMD > Conquest_out
+    echo "done" 
+    cd ..
+done
+
+# End timer
+end_time="$(date -u +%s.%N)"
+
+# Print elapsed time
+elapsed="$(bc <<<"$end_time-$start_time")"
+printf "total of %.3f seconds elapsed\n" "$elapsed"
---
+++ tutorials/loop_template/loop_string_runCQ_template.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Set env variables
+source ../../environment_variables.sh
+
+# Setup Conquest command
+CQCMD="$CQMPI $CQNUM $CQROOT/Conquest"
+echo "Conquest cmd: $CQCMD"
+
+# List of files to copy
+files=("Si.ion" "coords.dat" "Conquest_input")
+
+# List of cutoff energy values in Ha
+values=(50 75 100 120 150)
+
+# Folder basename
+base="gs_%03d"
+
+# String to edit in Conquest_input
+string="grid.gridcutoff "
+
+# Start timer
+start_time="$(date -u +%s.%N)"
+
+# Loop over each cutoff energy value
+for val in "${values[@]}"; do
+    folder=$(printf "$base" "$val")
+    if [ ! -d $folder ]; then
+       mkdir -p $folder
+    fi
+    # Copy required input files into the folder
+    cp "${files[@]}" $folder
+    cd $folder
+
+    # Generate Conquest_input file with current cutoff value
+    echo $string $val >> Conquest_input 
+
+    # Run Conquest in the folder
+    echo -n "Conquest is running in $folder... "
+    $CQCMD > Conquest_out
+    echo "done" 
+    cd ..
+done
+
+# End timer
+end_time="$(date -u +%s.%N)"
+
+# Print elapsed time
+elapsed="$(bc <<<"$end_time-$start_time")"
+printf "total of %.3f seconds elapsed\n" "$elapsed"
