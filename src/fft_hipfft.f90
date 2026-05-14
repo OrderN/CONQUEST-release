a33db3a49d9bc9aa20b3c0cc0e7c78409c1484bb
Tuomas Koskela <t.koskela@ucl.ac.uk>
2026-05-14 16:12:53 +0100

    bug fixes

--- src/fft_hipfft.f90
+++ src/fft_hipfft.f90
@@ -4,7 +4,7 @@
 ! 2019/05/29 16:54 dave
 !  Added real-to-real (i.e. cosine and sine) transforms
 module hipfft_interface_module
-  use iso_c_binding, only : c_ptr
+  use iso_c_binding, only : c_ptr, c_loc
   implicit none
 
   type(c_ptr), save :: plan
@@ -15,8 +15,9 @@ contains
     use hipfort_hipfft, only : hipfftplan3d_, HIPFFT_C2C
 
     integer, intent(in)  :: nsize
+    integer :: ierr
 
-    call hipfftplan3d_(plan, nsize, nsize, nsize, HIPFFT_C2C)
+    ierr = hipfftplan3d_(plan, nsize, nsize, nsize, HIPFFT_C2C)
   end subroutine hipfft3_init_wrapper
 
   subroutine hipfft3_exec_wrapper( cdata_h, nsize, isign )
@@ -47,7 +48,8 @@ contains
   end subroutine hipfft3_exec_wrapper
 
   subroutine hipfft3_dest_wrapper( )
-    call hipfftdestroy_(plan)
+    integer :: ierr
+    ierr = hipfftdestroy_(plan)
   end subroutine hipfft3_dest_wrapper
 
 end module hipfft_interface_module
