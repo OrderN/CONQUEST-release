7e6f2a99dcbf3e16e112891aefa59629e1a852cf
connoraird <c.aird@ucl.ac.uk>
2026-05-14 16:33:50 +0100

    Add openmp + hipfft example which reproduces pure hip example

---
+++ test_openmp_hipfft.f90
@@ -0,0 +1,73 @@
+program test_openmp_hipfft
+  use iso_c_binding, only : c_int, c_ptr, c_float, c_float_complex
+  use hipfort                      
+  use hipfort_hipfft               
+  implicit none
+
+  integer(c_int), parameter :: N = 1024
+  integer(c_int) :: i
+
+  type(c_ptr) :: plan
+  
+  integer, parameter :: double_cplx = selected_real_kind( 6, 70 )
+
+  ! Host arrays
+  complex(c_float_complex), allocatable, target :: x(:)
+  complex(c_float_complex), allocatable :: x_ref(:)
+  complex(c_float_complex), pointer :: x_d_ptr(:)
+
+  ! Error codes
+  integer :: ierr
+  integer :: fftstat
+  real(c_float) :: maxerr
+
+  allocate(x(N), x_ref(N))
+
+  ! Initialise a simple signal on host
+  do i = 1, N
+     x(i) = cmplx(real(i-1, c_float), -0.5_c_float, kind=c_float_complex)
+  end do
+  x_ref = x
+  
+  !$omp target data map(tofrom: x(1:N))
+    !$omp target data use_device_addr(x(1:N))
+      x_d_ptr => x
+
+      ! Create 1D FFT plan length N, complex-to-complex, batch=1 
+      fftstat = hipfftPlan1d(plan, N, HIPFFT_C2C, 1)
+      if (fftstat /= HIPFFT_SUCCESS) stop "hipfftPlan1d failed"
+
+      ! Forward FFT in-place (idata==odata is allowed) 
+      fftstat = hipfftExecC2C(plan, x_d_ptr, x_d_ptr, HIPFFT_FORWARD)
+      if (fftstat /= HIPFFT_SUCCESS) stop "hipfftExecC2C forward failed"
+
+      ! Backward (inverse) FFT in-place 
+      fftstat = hipfftExecC2C(plan, x_d_ptr, x_d_ptr, HIPFFT_BACKWARD)
+      if (fftstat /= HIPFFT_SUCCESS) stop "hipfftExecC2C backward failed"
+
+      fftstat = hipDeviceSynchronize_()
+      if (fftstat /= HIPFFT_SUCCESS) stop "hipDeviceSynchronize_ failed"
+
+    !$omp end target data
+  !$omp end target data
+
+  ! Normalise 
+  x = x / real(N, c_float)
+
+  ! Compute max error
+  maxerr = 0.0_c_float
+  do i = 1, N
+     maxerr = max(maxerr, abs(x(i) - x_ref(i)))
+  end do
+
+  write(*,'(a,i0)') "N = ", N
+  write(*,'(a,es12.4)') "max |x_back - x_ref| = ", maxerr
+
+  ! Cleanup
+  fftstat = hipfftDestroy(plan)    
+  !ierr    = hipFree(x_d)           
+
+  deallocate(x, x_ref)
+
+  write(*,*) "Finished hipfort + hipFFT forward/backward test"
+end program test_openmp_hipfft
